7faf26df13a4f4072b1f7249313829e9a5dd56a4
MariannaHon <imarianna96i@gmail.com>
2024-07-04 22:14:27 +0200

    commit
    after check

--- src/server.js
+++ src/server.js
@@ -34,6 +34,8 @@ export const setupServer = () => {
     app.get('/contacts', async (req, res) => {
         const contacts = await getAllContacts();
         res.status(200).json({
+            status: 200,
+            message: "Successfully found contacts!",
             data: contacts,
         });
     });
@@ -44,11 +46,14 @@ export const setupServer = () => {
 
         if (!contact) {
             res.status(404).json({
+                status: 404,
                 message: 'Contact not found'
             });
             return;
         }
         res.status(200).json({
+            status: 200,
+            message: `Successfully found contact with id ${contactId}!`,
             data: contact,
         });
     });
