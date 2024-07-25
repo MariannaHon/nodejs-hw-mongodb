c7bb3b7fb0c0f29c5d5923ffc4be0e07e05369ca
MariannaHon <imarianna96i@gmail.com>
2024-07-25 12:56:13 +0200

    commit
    after check

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,19 +1,16 @@
 
 
-
-import createHttpError from 'http-errors';
-
 export const validateBody = (schema) => async (req, res, next) => {
     try {
         await schema.validateAsync(req.body, {
             abortEarly: false,
         });
-        next();
-    } catch (err) {
-        const error = createHttpError(400, 'Bad Request', {
-            errors: err.details,
+    } catch (error) {
+        return res.status(400).json({
+            status: 400,
+            message: error.details[0].message,
         });
-        next(error);
     }
+    next();
 };
 
