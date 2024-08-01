b3f08ae87716d341624664f9aedbfc93a6898d2e
MariannaHon <imarianna96i@gmail.com>
2024-08-01 15:18:35 +0200

    ---

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,4 +1,5 @@
 
+import createHttpError from 'http-errors';
 
 export const validateBody = (schema) => async (req, res, next) => {
     try {
@@ -6,11 +7,12 @@ export const validateBody = (schema) => async (req, res, next) => {
             abortEarly: false,
         });
     } catch (error) {
-        return res.status(400).json({
-            status: 400,
-            message: error.details.map((err) => err.message).join(', '),
-        });
+        next(
+            createHttpError(
+                400,
+                error.details.map((err) => err.message).join(', '),
+            ),
+        );
     }
-    next();
 };
 
